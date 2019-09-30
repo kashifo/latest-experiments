fc5832b3da822044ea9a41c255ce0492ceb8ec36
kashifo <kashifanwaar@casperon.in>
2019-09-30 20:08:37 +0530

    MVVM Level 2: Recyclerview converted to databinding.

--- app/src/main/java/com/github/latestexperiments/mvvm_level2/CurrencyAdapter.java
+++ app/src/main/java/com/github/latestexperiments/mvvm_level2/CurrencyAdapter.java
@@ -1,21 +1,24 @@
 package com.github.latestexperiments.mvvm_level2;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.github.latestexperiments.R;
+import com.github.latestexperiments.databinding.ItemCurrencyCardBinding;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * Created by Kashif on 9/30/2019.
  */
-public class CurrencyAdapter extends RecyclerView.Adapter<CurrencyAdapter.MyViewHolder> {
+public class CurrencyAdapter extends RecyclerView.Adapter<CurrencyAdapter.MyViewHolder> implements CurrencyClickListener {
 
     private List<CurrencyPojo> currencyList;
 
@@ -34,28 +37,35 @@ public class CurrencyAdapter extends RecyclerView.Adapter<CurrencyAdapter.MyView
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_currency_card, parent, false);
-        return new MyViewHolder(view);
+        ItemCurrencyCardBinding binding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.getContext()),
+                R.layout.item_currency_card, parent, false);
+
+        return new MyViewHolder(binding);
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
-        TextView tvTitle, tvValue;
+        public ItemCurrencyCardBinding binding;
 
-        public MyViewHolder(View itemView) {
-            super(itemView);
-            this.tvTitle = (TextView) itemView.findViewById(R.id.tvCurrencyTitle);
-            this.tvValue = (TextView) itemView.findViewById(R.id.tvCurrencyValue);
+        public MyViewHolder(ItemCurrencyCardBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
         }
     }
 
     @Override
     public void onBindViewHolder(final MyViewHolder holder, final int listPosition) {
 
-        holder.tvTitle.setText(currencyList.get(listPosition).getName());
-        holder.tvValue.setText(currencyList.get(listPosition).getValue());
+        CurrencyPojo pojo = currencyList.get(listPosition);
+        holder.binding.setModel(pojo);
+        holder.binding.setItemClickListener(this);
 
+    }
+
+    @Override
+    public void onCurrencyClicked(CurrencyPojo pojo) {
+        Log.e("CurrencyAdapter", "onCurrencyClicked()-"+pojo.toString() );
     }
 
 }
---
+++ app/src/main/java/com/github/latestexperiments/mvvm_level2/CurrencyClickListener.java
@@ -0,0 +1,8 @@
+package com.github.latestexperiments.mvvm_level2;
+
+/**
+ * Created by Kashif on 9/30/2019.
+ */
+public interface CurrencyClickListener {
+    void onCurrencyClicked(CurrencyPojo pojo);
+}
